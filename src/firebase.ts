a871eb43ed4a094a5161487dd83103d3fa5c1c1a
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2024-01-21 11:07:25 +0100

    initialize firebase

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfqq8w6z4g_dlPn0Eey2HHjpLOMlcgUK4",
+  authDomain: "my-website-18197.firebaseapp.com",
+  projectId: "my-website-18197",
+  storageBucket: "my-website-18197.appspot.com",
+  messagingSenderId: "14974736336",
+  appId: "1:14974736336:web:7ffa8fc1520ca420654bd5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,48 +1,47 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
-// import { db } from "../firebase";
+import { db } from "../firebase";
 
 function About({ id }: { id: string }) {
-  const [bio1, setBio1] = useState("");
-  const [bio2, setBio2] = useState("");
-  const [bio3, setBio3] = useState("");
+  const [bio, setBio] = useState([]);
   const [imageUrl, setImageUrl] = useState("");
 
   // const storage = getStorage();
   // const imagesRef = ref(storage, "about.jpg");
 
-  // // Fetch about text and image URL from database
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       // Fetch text data from Firestore
-  //       // const querySnapshot = await getDocs(collection(db, "About"));
-  //       // const bioData = querySnapshot.docs.map((doc) => doc.data())[0];
+  // Fetch about text and image URL from database
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch text data from Firestore
+        const querySnapshot = await getDocs(collection(db, "Projects"));
+        const bioData = querySnapshot.docs.map((doc) => doc.data());
+        console.log(bioData);
 
-  //       // Fetch image URL from Firebase Storage
-  //       const url = await getDownloadURL(imagesRef);
-  //       setImageUrl(url);
-  //     } catch (error) {
-  //       console.error(
-  //         "Error connecting to Firestore or accessing Storage:",
-  //         error
-  //       );
-  //     }
-  //   };
+        // Fetch image URL from Firebase Storage
+        // const url = await getDownloadURL(imagesRef);
+        // setImageUrl(url);
+      } catch (error) {
+        console.error(
+          "Error connecting to Firestore or accessing Storage:",
+          error
+        );
+      }
+    };
 
-  //   // Call the function to fetch data
-  //   fetchData();
-  // }, [imagesRef]); // Dependency on imagesRef to re-fetch data when the image changes
+    // Call the function to fetch data
+    fetchData();
+  }, []); // Dependency on imagesRef to re-fetch data when the image changes
 
   return (
     <div className="h-screen border border-black" id={id}>
       <h1>About</h1>
       <div className="pt-4 grid gap-4 mx-auto md:w-4/5 xl:w-2/3">
         <img src={imageUrl} alt="About picture" />
-        <p>{bio1}</p>
+        {/* <p>{bio1}</p>
         <p>{bio2}</p>
-        <p>{bio3}</p>
+        <p>{bio3}</p> */}
       </div>
     </div>
   );
